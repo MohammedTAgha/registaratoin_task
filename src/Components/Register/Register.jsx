b6df4d93721a47e97728ee4323bef4a501162290
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-07-09 12:01:34 +0300

    add overlay to container

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-
+import {Container} from '../'
+import bg from '../../assets/bg.png'
 export default class Register extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Container width={"100vw"} height={"100vh"} row>
+  <Container width={"45%"} height={"100%"} backgroundImage={bg} overlayColor="rgba(0, 0, 255, 0.3)">
+    adsasdad
+  </Container>
+  <Container width={"65%"} backgroundImage={bg}>
+    adsasdad
+  </Container>
+</Container>
+
     )
   }
 }

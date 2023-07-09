b04f283aab5d3e643b22dfca8652c3b19c968754
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-07-09 11:17:28 +0300

    create container

---
+++ src/Components/Container/Container.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  width: ${(props) => props.width || "100%"};
+  display: flex;
+  flex-direction: ${(props) => (props.row ? "row" : "column")};
+  justify-content: ${(props) =>
+    props.justifyContent ? props.justifyContent : "center"};
+  gap: ${(props) =>
+    props.row ? `0 ${props.columnGap || "0"}` : `${props.rowGap || "0"} 0`};
+
+  align-items: center;
+  padding-top: ${(props) => props.paddingTop || "0"};
+  padding-right: ${(props) => props.paddingRight || "0"};
+  padding-bottom: ${(props) => props.paddingBottom || "0"};
+  padding-left: ${(props) => props.paddingLeft || "0"};
+  background-color: ${(props) =>
+    !props.overlay ? "transparent" : "var(--color-backgrounddark)"};
+  border: ${(props) => props.border || "none"};
+  border-radius: ${(props) => props.borderRadius || "0"};
+  box-shadow: ${(props) => props.boxShadow || "none"};
+`;
+
+export default class Container extends Component {
+  render() {
+    return <StyledContainer {...this.props} />;
+  }
+}

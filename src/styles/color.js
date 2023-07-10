b6176c88777409ec3e6d896d89ab4403d70a0b98
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-07-10 14:10:13 +0300

    add colors

---
+++ src/styles/color.js
@@ -0,0 +1,12 @@
+const colors = {
+    primary: '#1565D8',
+    secondary: '#fff',
+    tertiary: '#00DAF7',
+    heading: '#000',
+    body: '#fff',
+    label: '#8692A6',
+    warning: '#FFC107',
+  };
+  
+  export default colors;
+  
